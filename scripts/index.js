138a1f34b232bccb70a8357b0ce5a151295502f1
joel rivera <jorivera114@gmail.com>
2022-10-22 16:16:40 -0400

    l

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const intialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "../images/yosemite.png",
+  },
+  {
+    name: "Lake Louise",
+    link: "../images/lakelouise.png",
+  },
+  {
+    name: "Bald mountains",
+    link: "../images/baldmountains.png",
+  },
+  {
+    name: "Latemar",
+    link: "../images/latemare.png",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "../images/vanoise.png",
+  },
+  {
+    name: "Lago di Braies",
+    link: "../images/lagodibraise.png",
+  },
+];
