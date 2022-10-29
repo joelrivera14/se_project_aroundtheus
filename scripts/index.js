d038ee0533487247a62d7dc73fc407980f5b3f27
joel rivera <jorivera114@gmail.com>
2022-10-29 19:13:59 -0400

    k

--- scripts/index.js
+++ scripts/index.js
@@ -30,12 +30,12 @@ const openModal = document.querySelector(".profile__edit-button");
 const closeModal = document.querySelector(".modal__closebutton");
 function openClick() {
   console.log("you opened the element");
-  modalBox.classList.add(".modal_opened");
+  modalBox.classList.remove("modal_opened");
 }
 openModal.addEventListener("click", openClick);
 
 function closeClick() {
   console.log("you closed the element");
-  modalBox.classList.remove(".modal_opened");
+  modalBox.classList.add("modal_opened");
 }
 closeModal.addEventListener("click", closeClick);
