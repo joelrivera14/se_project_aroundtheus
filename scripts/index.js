6a4f5d12c39de775d27fcded501dde4f6826f5eb
joel rivera <jorivera114@gmail.com>
2022-10-29 18:55:12 -0400

    k

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,18 @@ const initialCards = [
     link: "../images/lagodibraise.png",
   },
 ];
+
+const modalBox = document.querySelector(".modal");
+const openModal = document.querySelector(".profile__edit-button");
+const closeModal = document.querySelector(".modal__closebutton");
+function openClick() {
+  console.log("you opened the element");
+  modalBox.classList.add(".modal_opened");
+}
+openModal.addEventListener("click", openClick);
+
+function closeClick() {
+  console.log("you closed the element");
+  modalBox.classList.remove(".modal_opened");
+}
+closeModal.addEventListener("click", closeClick);
